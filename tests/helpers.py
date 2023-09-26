f0f5e15aa34bb095627f8e1b96f11df2e30ff27f
Dan F-M <foreman.mackey@gmail.com>
2023-09-26 17:27:14 -0400

    adding some template tests

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,8 +1,10 @@
+import warnings
 from contextlib import contextmanager
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
 from copier import run_copy
+from copier.errors import DirtyLocalWarning
 from plumbum import local
 
 git = local["git"]
@@ -10,12 +12,14 @@ git = local["git"]
 
 @contextmanager
 def generate_project(**data):
-    data["project_name"] = data.get("project_name", "test project")
+    data["project_name"] = data.get("project_name", "project")
     data["project_description"] = data.get("project_name", "A test project")
     data["author_fullname"] = data.get("author_fullname", "A Python Coder")
     data["author_email"] = data.get("author_email", "coder@python.codes")
     with TemporaryDirectory() as project:
-        run_copy(".", project, data=data, defaults=True)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", DirtyLocalWarning)
+            run_copy(".", project, data=data, defaults=True, vcs_ref="HEAD")
         with local.cwd(project):
             git("init", ".")
             git("add", ".")
@@ -25,4 +29,5 @@ def generate_project(**data):
                 "--author=Test<test@test>",
                 "--no-verify",
             )
+            git("tag", "0.1.0")
             yield Path(project)
--- tests/test_template.py
+++ tests/test_template.py
@@ -1,3 +1,5 @@
+import platform
+
 import pytest
 from helpers import generate_project
 from plumbum import local
@@ -9,7 +11,12 @@ def test_pre_commit(extra_data):
         local["pre-commit"]("run", "--all-files")
 
 
-# @pytest.mark.parametrize("extra_data", [{}, {"c_or_cpp_support": True}])
-def test_template_tests():
-    with generate_project(c_or_cpp_support=True):
-        local.python("-m", "nox", "-s", "tests")
+@pytest.mark.parametrize("extra_data", [{}, {"c_or_cpp_support": True}])
+def test_template_tests(extra_data):
+    python_version = platform.python_version_tuple()
+    python_version = ".".join(python_version[:2])
+    print(python_version)
+    with generate_project(**extra_data):
+        local.python(
+            "-m", "nox", "--verbose", "--python", python_version, "-s", "tests"
+        )
